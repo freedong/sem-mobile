429c46d7ccd2f0dfc0cb67999f9dd48f1439bd6c
zxd <yrqc53719@163.com>
2018-09-06 18:32:26 +0800

    修改提交

--- app.js
+++ app.js
@@ -5,8 +5,13 @@ require("./css/sixpage.scss");
 
 let htmlWidth = document.documentElement.clientWidth || document.body.clientWidth;
 let htmlDom = document.getElementsByTagName('html')[0];
+console.log(htmlWidth);
+if (htmlWidth >=1024) {
+	htmlDom.style.fontSize = 102.4 + 'px';
+}else {
+	htmlDom.style.fontSize = htmlWidth / 10 + 'px';
+}
 
-htmlDom.style.fontSize = htmlWidth / 10 + 'px';
 
 
 window.addEventListener('resize',(e) => {
